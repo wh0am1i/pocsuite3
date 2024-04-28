3c7717147e0be62c16036e58beb98707aa530103
wh0am1i <you@example.com>
2024-04-28 15:09:18 +0800

    fix test action failed

--- tests/login_demo.py
+++ tests/login_demo.py
@@ -38,11 +38,10 @@ class DemoPOC(POCBase):
         result = {}
         payload = "username={0}&password={1}".format(self.get_option("username"), self.get_option("password"))
         r = requests.post(self.url, data=payload)
-        print(r.text)
-        # if r.status_code == 200:
-        result['VerifyInfo'] = {}
-        result['VerifyInfo']['URL'] = self.url
-        result['VerifyInfo']['Postdata'] = payload
+        if r.status_code == 200:
+            result['VerifyInfo'] = {}
+            result['VerifyInfo']['URL'] = self.url
+            result['VerifyInfo']['Postdata'] = payload
 
         return self.parse_output(result)
 
