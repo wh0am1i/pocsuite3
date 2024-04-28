aa5a0a10c3a547d2bf103e10d37a58f45b2a7418
wh0am1i <you@example.com>
2024-04-28 16:48:29 +0800

    fix test action failed

--- tests/test_httpserver.py
+++ tests/test_httpserver.py
@@ -79,10 +79,10 @@ class TestCase(unittest.TestCase):
                                 requestHandler=BaseRequestHandler)
             httpd.start()
             url = '{}://{}:{}/'.format('https', get_host_ip(), 6666)
-            requests.get(url)
+            requests.get(url, timeout=20)
         except requests.exceptions.SSLError:
             url = '{}://{}:{}/'.format('https', get_host_ip(), 6666)
-            resp = requests.get(url, verify=False)
+            resp = requests.get(url, verify=False, timeout=20)
             self.assertEqual(resp.status_code, 200)
         except Exception:
             assert False
